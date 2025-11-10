032783741c66a4ccec469efb02ce8bae7b83585b
Martin <sajat.martinkezi00@gmail.com>
2025-11-10 13:19:59 +0100

    cucucu

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -9,21 +9,25 @@
 
         <ion-card>
           <ion-card-content>
+
+            <!-- Teljes név -->
             <ion-item>
               <ion-input v-model="fullName" type="text" placeholder="Teljes név"></ion-input>
             </ion-item>
 
+            <!-- Email -->
             <ion-item>
               <ion-input v-model="email" type="email" placeholder="Email"></ion-input>
             </ion-item>
 
+            <!-- Jelszó -->
             <ion-item>
               <ion-input v-model="password" type="password" placeholder="Jelszó"></ion-input>
             </ion-item>
 
+            <!-- Beosztás select -->
             <ion-item>
-              <ion-label>Beosztás</ion-label>
-              <ion-select v-model="role" placeholder="Válassz beosztást">
+              <ion-select v-if="mounted" v-model="role" placeholder="Válassz beosztást">
                 <ion-select-option value="developer">Fejlesztő</ion-select-option>
                 <ion-select-option value="designer">Designer</ion-select-option>
                 <ion-select-option value="manager">Manager</ion-select-option>
@@ -31,12 +35,15 @@
               </ion-select>
             </ion-item>
 
-            <ion-button expand="block" @ionFocus="register" @click="register">Regisztráció</ion-button>
+            <ion-button expand="block" class="ion-margin-top" @click="register">
+              Regisztráció
+            </ion-button>
 
-            <p>
+            <p class="ion-text-center ion-margin-top">
               Van már fiókod?
               <router-link to="/" class="no-underline">Bejelentkezés</router-link>
             </p>
+
           </ion-card-content>
         </ion-card>
 
@@ -46,22 +53,28 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useFocus } from '@/composables/useFocus'
-import { IonPage, IonContent, IonItem, IonInput, IonButton, IonCard, IonCardContent } from '@ionic/vue'
+import { IonPage, IonContent, IonItem, IonInput, IonSelect, IonSelectOption, IonButton, IonCard, IonCardContent } from '@ionic/vue'
 
 export default {
   name: 'RegisterPage',
-  components: { IonPage, IonContent, IonItem, IonInput, IonButton, IonCard, IonCardContent },
+  components: { IonPage, IonContent, IonItem, IonInput, IonSelect, IonSelectOption, IonButton, IonCard, IonCardContent },
   setup() {
     useFocus()
+    
+    const mounted = ref(false)
+    onMounted(() => { mounted.value = true }) // IonSelect csak kliens oldalon jelenik meg
+
+    const fullName = ref('')
     const email = ref('')
     const password = ref('')
+    const role = ref('')
     const router = useRouter()
 
     function register() {
-      if(email.value && password.value){
+      if(fullName.value && email.value && password.value && role.value){
         localStorage.setItem('user', email.value)
         router.push('/home')
       } else {
@@ -69,7 +82,7 @@ export default {
       }
     }
 
-    return { email, password, register }
+    return { fullName, email, password, role, mounted, register }
   }
 }
 </script>
