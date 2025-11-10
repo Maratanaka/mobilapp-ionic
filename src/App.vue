e938c572c6c581645d7bd89b57cd079eb766333f
Martin <sajat.martinkezi00@gmail.com>
2025-11-10 15:48:56 +0100

    muzmzm

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <ion-app>
+  <ion-app class="force-light">
     <ion-router-outlet />
   </ion-app>
 </template>
@@ -7,3 +7,13 @@
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
+
+<style>
+.force-light {
+  --ion-background-color: #ffffff;
+  --ion-text-color: #000000;
+   --ion-card-background: #ffffff;
+    --ion-item-background: #ffffff;
+}
+
+</style>
